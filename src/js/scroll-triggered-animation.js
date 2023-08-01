3b5048ec2eeda8cc229ed18fdc4b1beb502962b3
Svitlana <wsv3ta@gmail.com>
2023-08-01 22:13:59 +0200

    Оновлено стилі
    Змінено дизайн кнопки при ховері та фокусі

--- src/js/scroll-triggered-animation.js
+++ src/js/scroll-triggered-animation.js
@@ -9,21 +9,5 @@ const observer = new IntersectionObserver((entries) => {
     });
 }); 
 
-
 const hiddenElements = document.querySelectorAll('.hidden-title');
 hiddenElements.forEach((el) => observer.observe(el));
-// 
-const observer2 = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show-button');
-        } else {
-            entry.target.classList.remove('show-button');
-        }
-    });
-}); 
-
-
-const hiddenElements2 = document.querySelectorAll('.hidden-button');
-hiddenElements2.forEach((el) => observer2.observe(el));
